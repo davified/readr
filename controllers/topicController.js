0ca9b2903352bdb715a7cc1a5ec76567320e6101
David Tan <davified@gmail.com>
2016-07-21 16:08:01 +0800

    david's commit on thursday 4pm

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -47,12 +47,41 @@ function removeTopic (req, res) {
 function getAllArticlesOfOneTopic (req, res) {
   const topicName = req.params.name
   Topic.findOne({topic: topicName}, function (err, topic) {
-    if (err || !topic) return res.status(404).json({error: '/topics getAllArticlesOfOneTopic() error 1'})
+    if (err || !topic) return res.status(404).json({error: '/topics/:name getAllArticlesOfOneTopic() error 1'})
     Article
       .find({topics: topic.id}, function (err, articlesArray) {
-        if (err) return res.status(404).json({error: '/topics getAllArticlesOfOneTopic() error 2'})
+        if (err) return res.status(404).json({error: '/topics/:name getAllArticlesOfOneTopic() error 2'})
         res.status(200).json(articlesArray)
       })
+  })
+}
+
+// creating a function to get all articles of all topics. it NEEDS to return an array of articles.
+function getAllArticlesOfAllTopics (req, res) {
+  const articlesByTopicArray = []
+  Topic.find({}, function (err, topicsArray) {
+    if (err) return res.status(401).json({error: '/topics getAllArticlesOfAllTopics() error 1'})
+    topicsArray.forEach(function (topic) {
+      Article
+        .find({topics: topic}, function (err, articlesArray) {
+          if (err) return res.status(404).json({error: '/topics/:name getAllArticlesOfOneTopic() error 2'})
+          articlesArray.forEach(function (article) {
+            articlesByTopicArray.push(article)
+          // console.log(article)
+          })
+        // console.log(articlesByTopicArray)
+        })
+    })
+  })
+  res.status(200).json(articlesByTopicArray) // why am i not able to get articlesByTopicArray on chrome? I can see it in the console..
+}
+
+function findByQuery (req, res) {
+  var query = req.query
+  db.collection('artists', function (err, collection) {
+    collection.find(query).toArray(function (err, docs) {
+      res.send(docs)
+    })
   })
 }
 
@@ -62,5 +91,7 @@ module.exports = {
   getTopic: getTopic,
   updateTopic: updateTopic,
   removeTopic: removeTopic,
-  getAllArticlesOfOneTopic: getAllArticlesOfOneTopic
+  getAllArticlesOfOneTopic: getAllArticlesOfOneTopic,
+  getAllArticlesOfAllTopics: getAllArticlesOfAllTopics,
+  findByQuery: findByQuery
 }
